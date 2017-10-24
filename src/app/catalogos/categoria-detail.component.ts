a03fd49a210f1239ccb75b3145f989662a25012f
Alex <alxrex@users.noreply.github.com>
2017-10-24 12:02:44 -0500

    customized to Categoria
    Removed Hero

--- src/app/catalogos/categoria-detail.component.ts
+++ src/app/catalogos/categoria-detail.component.ts
@@ -4,30 +4,30 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location }                 from '@angular/common';
 
 import { Categoria }        from './Categoria';
-import { CategoriaService } from './Categoria.service';
+import { CategoriasService } from './Categorias.service';
 
 @Component({
   selector: 'caegoria-detail',
   templateUrl: './categoria-detail.component.html',
   styleUrls: [ './categoria-detail.component.css' ]
 })
-export class HeroDetailComponent implements OnInit {
-  categoria: Hero;
+export class CategoriaDetailComponent implements OnInit {
+	categoria: Categoria;
 
   constructor(
-    private categoriaService: HeroService,
+    private categoriasService: CategoriasService,
     private route: ActivatedRoute,
     private location: Location
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap
-      .switchMap((params: ParamMap) => this.categoriaService.getHero(+params.get('id')))
+      .switchMap((params: ParamMap) => this.categoriasService.getCategoria(+params.get('id')))
       .subscribe(categoria => this.categoria = categoria);
   }
 
   save(): void {
-    this.categoriaService.update(this.categoria)
+    this.categoriasService.update(this.categoria)
       .then(() => this.goBack());
   }
 
