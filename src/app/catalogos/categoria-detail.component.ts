58b9853439fa7ab56477243d2b7f3a8fdf9c3448
Alex Aceves <aceves.dimax@gmail.com>
2017-10-28 00:34:24 -0500

    Resolve duplicated

--- src/app/catalogos/categoria-detail.component.ts
+++ src/app/catalogos/categoria-detail.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'rxjs/add/operator/switchMap';
 import { Component, OnInit }        from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
@@ -19,7 +18,9 @@ export class CategoriaDetailComponent implements OnInit {
     private categoriasService: CategoriasService,
     private route: ActivatedRoute,
     private location: Location
-  ) {}
+  ) {
+    console.log("Categorias");
+  }
 
   ngOnInit(): void {
     this.route.paramMap
@@ -36,42 +37,3 @@ export class CategoriaDetailComponent implements OnInit {
     this.location.back();
   }
 }
-=======
-import 'rxjs/add/operator/switchMap';
-import { Component, OnInit }        from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Location }                 from '@angular/common';
-
-import { Categoria }        from './Categoria';
-import { CategoriasService } from './Categorias.service';
-
-@Component({
-  selector: 'caegoria-detail',
-  templateUrl: './categoria-detail.component.html',
-  styleUrls: [ './categoria-detail.component.css' ]
-})
-export class CategoriaDetailComponent implements OnInit {
-	categoria: Categoria;
-
-  constructor(
-    private categoriasService: CategoriasService,
-    private route: ActivatedRoute,
-    private location: Location
-  ) {}
-
-  ngOnInit(): void {
-    this.route.paramMap
-      .switchMap((params: ParamMap) => this.categoriasService.getCategoria(+params.get('id')))
-      .subscribe(categoria => this.categoria = categoria);
-  }
-
-  save(): void {
-    this.categoriasService.update(this.categoria)
-      .then(() => this.goBack());
-  }
-
-  goBack(): void {
-    this.location.back();
-  }
-}
->>>>>>> a03fd49a210f1239ccb75b3145f989662a25012f
